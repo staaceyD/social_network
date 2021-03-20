db92832c4fb98e87a73cc18569d8d2cc393e80a4
Anastasiia Derevianko <anastasiia1010@gmail.com>
2021-03-20 11:21:19 +0200

    create apis for post and likes (#2)
    Co-authored-by: anastasiia_derevianko

--- social_network/post/views.py
+++ social_network/post/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import PostSerializer, PostLikeSerializer, PostDislikeSerializer
+from .models import Post
 
-# Create your views here.
+
+class PostCreateApi(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostReadApi(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostUpdateApi(generics.UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDeleteApi(generics.DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetailApi(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class LikeApiView(generics.UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostLikeSerializer
+
+
+class DislikeApiView(generics.UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDislikeSerializer
