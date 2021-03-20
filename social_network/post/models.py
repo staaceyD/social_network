f3b268a7ba94ea7f0d2d636042f9ecf2aa38da63
anastasiia_derevianko <anderevyanko@provectus.com>
2021-03-20 10:15:13 +0200

    create post model

--- social_network/post/models.py
+++ social_network/post/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author_name = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="post")
+    creation_date = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0, editable=False)
