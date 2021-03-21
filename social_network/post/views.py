9cbafd1c9ac77093bfc330ca52b2151f5becb128
anastasiia_derevianko <anderevyanko@provectus.com>
2021-03-21 11:30:00 +0200

    create register, login, refresh token APIs

--- social_network/post/views.py
+++ social_network/post/views.py
@@ -1,38 +1,46 @@
 from rest_framework import generics
 from .serializers import PostSerializer, PostLikeSerializer, PostDislikeSerializer
 from .models import Post
+from rest_framework.permissions import IsAuthenticated
 
 
 class PostCreateApi(generics.CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class PostReadApi(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class PostUpdateApi(generics.UpdateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class PostDeleteApi(generics.DestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class PostDetailApi(generics.RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class LikeApiView(generics.UpdateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostLikeSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class DislikeApiView(generics.UpdateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostDislikeSerializer
+    permission_classes = [IsAuthenticated]
